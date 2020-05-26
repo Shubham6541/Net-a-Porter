e78382fc075ba963d16d206739f38f0148ba626c
Kuamr Shubham <Kumar.shubham@navi.com>
2020-05-26 19:06:41 +0530

    Add cache-manager to refresh cache

---
+++ server/controllers/cache-manager.js
@@ -0,0 +1,24 @@
+const flatCache = require('flat-cache');
+const path = require('path');
+const cache = flatCache.load('productsCache', path.resolve('../cache'));
+const Product = require('../database/models/product');
+const responseData = require('./product-controller');
+
+function clearCache() {
+    console.log("Celaring");
+    flatCache.clearAll();
+};
+
+function fillCache() {
+    Product.find().then(items=>{
+        let data = [];
+        items.map(item => data.push(responseData(item)));
+        cache.setKey('__express__[]',data);
+        cache.save();
+    })
+};
+
+setInterval(async ()=>{
+    await clearCache();
+    await fillCache();
+    }, 360000);
