61bfa4d335908adf2aef2f2bb5ab7126fe1dfe8a
Kuamr Shubham <Kumar.shubham@navi.com>
2020-05-26 12:13:03 +0530

    Add Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const Header = () => {
+    return (
+        <div>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant="h6">NAP Store</Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+};
+
+export default Header;
