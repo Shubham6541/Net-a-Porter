c682514f2f1b70fb18e078d81d6b7bf3bb34e7ab
Kuamr Shubham <Kumar.shubham@navi.com>
2020-05-26 19:07:01 +0530

    Add configuration file

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    DATABASE_URL: 'mongodb://shubham:shubham@green-deck-shard-00-00-3uwoo.mongodb.net:27017,green-deck-shard-00-01-3uwoo.mongodb.net:27017,green-deck-shard-00-02-3uwoo.mongodb.net:27017/test?ssl=true&replicaSet=Green-deck-shard-0&authSource=admin&retryWrites=true&w=majority',
+    LOAD_DATA_URI: "https://greendeck-datasets-2.s3.amazonaws.com/netaporter_gb_similar.json",
+    PORT: 8080
+};
