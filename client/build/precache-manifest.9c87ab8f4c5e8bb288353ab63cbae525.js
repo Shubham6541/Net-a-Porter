5c2a266dce0e285d4eb7dbdbf3903c9b68d4d1d9
Kuamr Shubham <Kumar.shubham@navi.com>
2020-05-30 13:52:01 +0530

    Add deployment specific files

---
+++ client/build/precache-manifest.9c87ab8f4c5e8bb288353ab63cbae525.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c096003f3b047cec9bccb036c0bbd22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a8e4f0218aba9c0500b",
+    "url": "/static/css/main.5c2b6894.chunk.css"
+  },
+  {
+    "revision": "5fadc320704a44dac4be",
+    "url": "/static/js/2.6cbd6566.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6cbd6566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd88ea3d95cb738a95d5",
+    "url": "/static/js/3.ae3bf370.chunk.js"
+  },
+  {
+    "revision": "9a78d0ccfc83b0c7cf13",
+    "url": "/static/js/4.d0290dad.chunk.js"
+  },
+  {
+    "revision": "47ec1d6f4ae13ac07cdf",
+    "url": "/static/js/5.6fa1c629.chunk.js"
+  },
+  {
+    "revision": "93f19747a447b503012d",
+    "url": "/static/js/6.64774b13.chunk.js"
+  },
+  {
+    "revision": "4a8e4f0218aba9c0500b",
+    "url": "/static/js/main.aa4b3cca.chunk.js"
+  },
+  {
+    "revision": "2a9105676023f7ab83f5",
+    "url": "/static/js/runtime-main.192a3c71.js"
+  }
+]);
